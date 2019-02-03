a0644c04bf8283308f0fb170b0e8f198f3e52a0f
George  Pianka <gpianka1@gmail.com>
2019-02-03 04:36:47 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait_validator.rb
+++ app/models/concerns/clickbait_validator.rb
@@ -1,7 +1,7 @@
 class ClickbaitValidator < ActiveModel::Validator
   
   def validate(title)
-    
+    clickbait_patterns = %w()
     if clickbait_patterns.none? { |pat| pat.match title }
   end
   
