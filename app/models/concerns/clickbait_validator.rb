19fd526939bfe390d9a480863ddea5cabbb7c8c6
George  Pianka <gpianka1@gmail.com>
2019-02-03 05:13:37 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait_validator.rb
+++ app/models/concerns/clickbait_validator.rb
@@ -3,7 +3,7 @@ class ClickbaitValidator < ActiveModel::Validator
   def validate
     patterns = %r{Won't Believe|Secret|Guess|Top [0-9]*}i
     if title !~ patterns
-    errors.add :title, "Must be Clickbait"
+    errors.add :title, "Must be Clickbait!"
     end
   end
 end
