19c86713b30a40715a856757ee370c1a8fee378f
George  Pianka <gpianka1@gmail.com>
2019-02-03 04:05:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true, clickbait: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum:250 }
-  validates :category, inclusion: { in: %w(Non-Fiction Fiction) }
+  validates :category, inclusion: {in: %w(Non-Fiction Fiction)}
 end
