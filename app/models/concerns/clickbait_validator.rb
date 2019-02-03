7b27c7caef0832751f5661c4cf4d352f10dfaa11
George  Pianka <gpianka1@gmail.com>
2019-02-03 05:10:51 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait_validator.rb
+++ app/models/concerns/clickbait_validator.rb
@@ -1,9 +1,9 @@
 class ClickbaitValidator < ActiveModel::Validator
   
-  def validate(title)
+  def validate()
     patterns = %r{Won't Believe|Secret|Guess|Top [0-9]*}i
     if title !~ patterns
-    errors.add(:density, “is too high to safely ship”)
+    errors.add(:title, “is too high to safely ship”)
     end
   end
 end
