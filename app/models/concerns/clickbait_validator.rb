466dd51434484879f20bd4a5e4ffa67a67d55b37
George  Pianka <gpianka1@gmail.com>
2019-02-03 04:58:05 +0000

    Automatically backed up by Learn

--- app/models/concerns/clickbait_validator.rb
+++ app/models/concerns/clickbait_validator.rb
@@ -1,14 +1,13 @@
 class ClickbaitValidator < ActiveModel::Validator
   
   def validate(title)
-    patterns = %r{Won't Believe,
+    patterns = %r{Won't Believe|}
     if patterns !=~ title
   end
+end
     
 #     /Secret/i,
 #     /Top [0-9]*/i,
 #     /Guess/i)
-    if patterns.none? {|pat| pat.match title }
-  end
   
-end
+  
